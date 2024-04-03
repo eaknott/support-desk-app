63581191f81d862fac4185fbd1a38cfe13d04338
eaknott <emknotty@gmail.com>
2024-04-03 20:20:44 +0800

    Add Spinner component for when page is loading, and add login functionality, checking user credentials in backend and navigating user to Home page while keeping userData in redux state

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,10 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { FaSignInAlt } from 'react-icons/fa'
 import { toast } from 'react-toastify'
 import { useSelector, useDispatch } from 'react-redux'
-import { login } from '../features/auth/authSlice'
+import { login, reset } from '../features/auth/authSlice'
+import { useNavigate } from 'react-router-dom'
+import Spinner from '../components/Spinner'
 
 function Login() {
   const [formData, setFormData] = useState({
@@ -13,17 +15,31 @@ function Login() {
   const { email, password } = formData
 
   const dispatch = useDispatch()
+  const navigate = useNavigate()
 
-  const { user, isLoading, isSuccess, message } = useSelector(
+  const { user, isError, isLoading, isSuccess, message } = useSelector(
     (state) => state.auth
   )
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message)
+    }
+
+    // Redirect when logged in
+    if (isSuccess || user) {
+      navigate('/')
+    }
+
+    dispatch(reset())
+  },[isError, isSuccess, user, message, navigate, dispatch])
 
   const onSubmit = (e) => {
     e.preventDefault()
 
     const userData = {
       email, 
-      password
+      password,
     }
 
     dispatch(login(userData))
@@ -34,6 +50,10 @@ function Login() {
       ...prev, 
       [e.target.name]: e.target.value
     }))
+  }
+
+  if (isLoading) {
+    return <Spinner />
   }
 
   return (
