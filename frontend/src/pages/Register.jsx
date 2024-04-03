f8cea8c5eacfc17591391a2a16d34545980972e0
eaknott <emknotty@gmail.com>
2024-04-03 17:34:31 +0800

    Add useEffect to check if user already exists, otherwise register new user and navigate back to Home page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { FaUser } from 'react-icons/fa'
 import { toast } from 'react-toastify'
 import { useSelector, useDispatch } from 'react-redux'
-import { register } from '../features/auth/authSlice'
+import { register, reset } from '../features/auth/authSlice'
 
 function Register() {
   const [formData, setFormData] = useState({
@@ -15,8 +16,22 @@ function Register() {
   const { name, email, password, password2 } = formData
 
   const dispatch = useDispatch()
+  const navigate = useNavigate()
 
-  const { user, isLoading, isSuccess, message } = useSelector(state => state.auth)
+  const { user, isLoading, isError, isSuccess, message } = useSelector(state => state.auth)
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message)
+    }
+
+    // Redirect when logged in
+    if (isSuccess && user) {
+      navigate('/')
+    }
+
+    dispatch(reset())
+  },[isError, isSuccess, user, message, navigate, dispatch])
 
   const onSubmit = (e) => {
     e.preventDefault()
