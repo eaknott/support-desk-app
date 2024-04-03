b26da470b1f901c4ff89bc4ca445ab0bd2ddaf7e
eaknott <emknotty@gmail.com>
2024-04-03 15:26:06 +0800

    Add relevant forms and buttons for Home, Register, and Login pages

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,51 @@
+import { useState } from 'react'
+import { FaSignInAlt } from 'react-icons/fa'
+import { toast } from 'react-toastify'
+
 function Login() {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  })
+
+  const { email, password } = formData
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+  }
+
+  const onChange = (e) => {
+    setFormData((prev) => ({
+      ...prev, 
+      [e.target.name]: e.target.value
+    }))
+  }
+
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <>
+    <section className="heading">
+      <h1>
+        <FaSignInAlt /> Login
+      </h1>
+      <p>Please log in to get support</p>
+    </section>
+
+    <section className="form">
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <input type="email" className="form-control" id="email" name="email" value={email} onChange={onChange} placeholder="Enter your email" required />
+        </div>
+        <div className="form-group">
+          <input type="password" className="form-control" id="password" name="password" value={password} onChange={onChange} placeholder="Enter password" required />
+        </div>
+        <div className="form-group">
+          <button className="btn btn-block">Submit</button>
+        </div>
+
+      </form>
+    </section>
+    </>
   )
 }
 
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,8 +1,62 @@
+import { useState } from 'react'
+import { FaUser } from 'react-icons/fa'
+import { toast } from 'react-toastify'
+
 function Register() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  })
+
+  const { name, email, password, password2 } = formData
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    if (password !== password2) {
+      toast.error('Passwords do not match')
+    }
+  }
+
+  const onChange = (e) => {
+    setFormData((prev) => ({
+      ...prev, 
+      [e.target.name]: e.target.value
+    }))
+  }
+
   return (
-    <div>
-      <h1>Register</h1>
-    </div>
+    <>
+    <section className="heading">
+      <h1>
+        <FaUser /> Register
+      </h1>
+      <p>Please create an account</p>
+    </section>
+
+    <section className="form">
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <input type="text" className="form-control" id="name" name="name" value={name} onChange={onChange} placeholder="Enter your name" required />
+        </div>
+        <div className="form-group">
+          <input type="email" className="form-control" id="email" name="email" value={email} onChange={onChange} placeholder="Enter your email" required />
+        </div>
+        <div className="form-group">
+          <input type="password" className="form-control" id="password" name="password" value={password} onChange={onChange} placeholder="Enter password" required />
+        </div>
+        <div className="form-group">
+          <input type="password" className="form-control" id="password2" name="password2" value={password2} onChange={onChange} placeholder="Confirm password" required />
+        </div>
+        <div className="form-group">
+          <button className="btn btn-block">Submit</button>
+        </div>
+
+      </form>
+    </section>
+    </>
   )
 }
 
