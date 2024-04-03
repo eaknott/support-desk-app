9d0f411e293fec51426f53687b788beb55581abe
eaknott <emknotty@gmail.com>
2024-04-03 16:04:02 +0800

    Use createAsyncThunk for register and login functions to bring into Register and Login pages to manage state with redux

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import { FaSignInAlt } from 'react-icons/fa'
 import { toast } from 'react-toastify'
+import { useSelector, useDispatch } from 'react-redux'
+import { login } from '../features/auth/authSlice'
 
 function Login() {
   const [formData, setFormData] = useState({
@@ -10,9 +12,21 @@ function Login() {
 
   const { email, password } = formData
 
+  const dispatch = useDispatch()
+
+  const { user, isLoading, isSuccess, message } = useSelector(
+    (state) => state.auth
+  )
+
   const onSubmit = (e) => {
     e.preventDefault()
 
+    const userData = {
+      email, 
+      password
+    }
+
+    dispatch(login(userData))
   }
 
   const onChange = (e) => {
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import { FaUser } from 'react-icons/fa'
 import { toast } from 'react-toastify'
+import { useSelector, useDispatch } from 'react-redux'
+import { register } from '../features/auth/authSlice'
 
 function Register() {
   const [formData, setFormData] = useState({
@@ -12,11 +14,23 @@ function Register() {
 
   const { name, email, password, password2 } = formData
 
+  const dispatch = useDispatch()
+
+  const { user, isLoading, isSuccess, message } = useSelector(state => state.auth)
+
   const onSubmit = (e) => {
     e.preventDefault()
 
     if (password !== password2) {
       toast.error('Passwords do not match')
+    } else {
+      const userData = {
+        name,
+        email,
+        password
+      }
+
+      dispatch(register(userData))
     }
   }
 
