cded009d30768759671cbae900dded8a6e2ebe8a
eaknott <emknotty@gmail.com>
2024-04-05 16:02:36 +0800

    Add function to get/show all user tickets, and add extraReducer cases for pending, fulfilled, and rejected

--- frontend/src/features/tickets/ticketSlice.js
+++ frontend/src/features/tickets/ticketSlice.js
@@ -30,6 +30,26 @@ export const createTicket = createAsyncThunk(
     }
 )
 
+// Get user tickets
+export const getTickets = createAsyncThunk(
+    'tickets/get-all', 
+    async (_, thunkAPI) => {
+        try {
+            const token = thunkAPI.getState().auth.user.token
+            return await ticketService.getTickets(token)
+        } catch (error) {
+            const message = 
+            (error.response && 
+                error.response.data && 
+                error.response.data.message) || 
+            error.message || 
+            error.toString()
+
+            return thunkAPI.rejectWithValue(message)
+        }
+    }
+)
+
 export const ticketSlice = createSlice({
     name: 'ticket',
     initialState,
@@ -50,7 +70,20 @@ export const ticketSlice = createSlice({
                 state.isError = true
                 state.message = action.payload
             })
-    },
+            .addCase(getTickets.pending, (state) => {
+                state.isLoading = true
+            })
+            .addCase(getTickets.fulfilled, (state, action) => {
+                state.isLoading = false
+                state.isSuccess = true
+                state.tickets = action.payload
+            })
+            .addCase(getTickets.rejected, (state, action) => {
+                state.isLoading = false
+                state.isError = true
+                state.message = action.payload
+            })
+        },
 })
 
 export const { reset } = ticketSlice.actions
