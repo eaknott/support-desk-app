9b13cdceb13a0356441e79ebf511682956e81f46
eaknott <emknotty@gmail.com>
2024-04-05 16:04:26 +0800

    Add redux functionality with useDispatch and useSelector

---
+++ frontend/src/pages/Tickets.jsx
@@ -0,0 +1,35 @@
+import { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { getTickets, reset } from '../features/tickets/ticketSlice'
+import Spinner from '../components/Spinner'
+import BackButton from '../components/BackButton'
+
+function Tickets() {
+    const { tickets, isLoading, isSuccess } = useSelector((state) => state.tickets)
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        return () => {
+            if (isSuccess) {
+                dispatch(reset())
+            }
+        }
+    }, [dispatch, isSuccess])
+
+    useEffect(() => {
+        dispatch(getTickets())
+    }, [dispatch])
+
+    if (isLoading) {
+        return <Spinner />
+    }
+
+  return (
+    <div>
+      Tickets
+    </div>
+  )
+}
+
+export default Tickets
