152d1debe8b1e1128d70c03c851fc302fb123dad
eaknott <emknotty@gmail.com>
2024-04-05 13:36:59 +0800

    Add asyncHandler functions to get user tickets and to create a new ticket

---
+++ backend/controllers/ticketController.js
@@ -0,0 +1,24 @@
+const asyncHandler = require('express-async-handler')
+
+const User = require('../models/userModel')
+const Ticket = require('../models/ticketModel')
+
+// @desc    Get user tickets
+// @route   GET /api/tickets
+// @access  Private
+const getTickets = asyncHandler(async (req, res) => {
+    res.status(200).json({message: 'getTickets'})
+})
+
+// @desc    Create new ticket
+// @route   POST /api/tickets
+// @access  Private
+const createTicket = asyncHandler(async (req, res) => {
+    res.status(200).json({message: 'createTickets'})
+})
+
+
+module.exports = {
+    getTickets,
+    createTicket,
+}
