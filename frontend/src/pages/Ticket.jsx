e84a21e578fb080f024e8bdf191e05b22b5bd0c2
eaknott <emknotty@gmail.com>
2024-04-05 23:19:24 +0800

    Add product name for ticket, add a button to close the ticket if the ticket status is not 'closed', and add an onClick function to close the ticket, changing the ticket status to 'closed'

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -1,16 +1,18 @@
 import { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { getTicket, reset } from '../features/tickets/ticketSlice'
-import { useParams } from 'react-router-dom'
+import { getTicket, reset, closeTicket } from '../features/tickets/ticketSlice'
+import { useParams, useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import BackButton from '../components/BackButton'
 import Spinner from '../components/Spinner'
+
 
 function Ticket() {
     const { ticket, isLoading, isSuccess, isError, message } = useSelector((state) => state.tickets)
 
     const params = useParams()
     const dispatch = useDispatch()
+    const navigate = useNavigate()
     const { ticketId } = useParams()
 
     useEffect(() => {
@@ -21,6 +23,13 @@ function Ticket() {
         dispatch(getTicket(ticketId))
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [isError, message, ticketId])
+
+    // Close ticket onClick button event
+    const onTicketClose = () => {
+        dispatch(closeTicket(ticketId))
+        toast.success('Ticket Closed')
+        navigate('/tickets')
+    }
 
     if (isLoading) {
         return <Spinner />
@@ -43,12 +52,19 @@ function Ticket() {
             <h3>
                 Date Submitted: {new Date(ticket.createdAt).toLocaleString('en-US')}
             </h3>
+            <h3>
+                Product: {ticket.product}
+            </h3>
             <hr />
             <div className="ticket-desc">
                 <h3>Description of Issue</h3>
                 <p>{ticket.description}</p>
             </div>
         </header>
+
+        {ticket.status !== 'closed' && (
+            <button onClick={onTicketClose} className="btn btn-block btn-danger">Close Ticket</button>
+        )}
     </div>
   )
 }
