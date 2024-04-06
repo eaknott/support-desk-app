626e5aae09c95f1bc72de148076871837cc5cffa
eaknott <emknotty@gmail.com>
2024-04-06 11:12:25 +0800

    Create noteModel, noteRoutes, and noteController and add a re-route into note router in ticketRoutes for backend staff writing notes on tickets

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -4,6 +4,10 @@ const { getTickets, getTicket, createTicket, deleteTicket, updateTicket } = requ
 
 const { protect }  = require('../middleware/authMiddleware')
 
+// Re-route into note router
+const noteRouter = require('./noteRoutes')
+router.use('/:ticketId/notes', noteRouter)
+
 router.route('/').get(protect, getTickets).post(protect, createTicket)
 
 router.route('/:id').get(protect, getTicket).delete(protect, deleteTicket).put(protect, updateTicket)
